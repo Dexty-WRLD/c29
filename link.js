c537d6650840d46ebd81eddc48a4005dc480c1ce
Dexty-WRLD <78935571+Dexty-WRLD@users.noreply.github.com>
2022-01-16 12:41:54 -0500

    Add files via upload

---
+++ link.js
@@ -0,0 +1,15 @@
+class Link{
+    constructor(bodyA,bodyB){
+        var lastLink = bodyA.body.bodies.length - 2;
+        var options = {
+            bodyA:bodyA.body.bodies[lastLink],
+            bodyB:bodyB,
+            pointA:{x:0,y:0},
+            pointB:{x:0,y:0},
+            length:-10,
+            stiffness:0.01
+        }
+        this.link = Constraint.create(options)
+        World.add (world,this.link);
+    }
+}
